fb00d6d18ca613c2cb41feb3a7c3e8ac5c0e6a28
filipMaM <filip.mamcarczyk@gmail.com>
2021-01-21 19:36:42 +0100

    Ep 1

---
+++ 1 - console/1/app.js
@@ -0,0 +1,24 @@
+console.log("Hej!");
+console.log("Kup se klej!")
+
+console.log(1)
+console.log(21 + 37)
+
+let currentYear = "2021"
+
+console.log("2021")
+
+let name = "Filip"
+let lastName = "Mamcarczyk"
+let age = 18
+
+console.log("name", name)
+console.log("lastName", lastName)
+console.log("age", age)
+
+console.log({name, lastName, age})
+
+console.table
+
+console.alert
+
